dec75a38fdf8f3e661b80358d9d5746eef20dd1e
edoh <edoh.dev@gmail.com>
2019-04-27 20:56:39 +0200

    add footer and header

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-import {MatCardModule, MatButtonModule} from '@angular/material';
+import {MatCardModule, MatButtonModule, MatToolbarModule} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -17,7 +17,8 @@ import {MatCardModule, MatButtonModule} from '@angular/material';
     BrowserAnimationsModule,
     HttpClientModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
